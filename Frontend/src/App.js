00ec7345a012eb95e5f0cd359f81adb74203763c
Infinity1105 <harshkulkarni1105@gmail.com>
2023-04-20 07:25:06 +0530

    Preloader Commit
    Added Preloader to App

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,29 +1,43 @@
-import * as React from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ChakraProvider, Image } from "@chakra-ui/react";
 import Navbar from "./Navbar";
 import Home from "./Home";
-import Demo from "./Demo";
 import { extendTheme } from "@chakra-ui/react";
 import "@fontsource/source-code-pro/800.css";
+import Preloader from "./Preloader";
 // import { Fonts } from "./Fonts";
 const theme = extendTheme({
   fonts: {
     body: "Source Code Pro,monospace",
-    heading: "Source Code Pro,monospace"
+    heading: "Source Code Pro,monospace",
   },
 });
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Delay state update to remove preloader
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2500);
+
+    // Clear timeout on unmount
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <Router>
-      <ChakraProvider theme={theme}>
+    <ChakraProvider theme={theme}>
+      <Router>
         <Navbar />
-        <Routes>
-          <Route path="/" exact element={<Home />} />
-          <Route path="/demo" exact element={<Demo />} />
-        </Routes>
-      </ChakraProvider>
-    </Router>
+        {loading && <Preloader />}
+        {!loading && (
+          <Routes>
+            <Route path="/" exact element={<Home />} />
+          </Routes>
+        )}
+      </Router>
+    </ChakraProvider>
   );
 }
 export default App;
---
+++ Frontend/src/Preloader.js
@@ -0,0 +1,17 @@
+import { Center, Image } from "@chakra-ui/react";
+import pl from "./Gifs/NewPreloader2.mp4";
+
+const Preloader = () => {
+  return (
+    <Center height="100vh">
+      <video
+        style={{ filter: "brightness(100%)" }}
+        src={pl}
+        autoPlay
+        muted
+      ></video>
+    </Center>
+  );
+};
+
+export default Preloader;
